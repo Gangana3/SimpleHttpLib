dce7100f8d32dbd9872c439f7aab934d5f0f5113
Gangana3 <organy110@gmail.com>
2018-02-22 14:01:56 +0200

    added __get_code function in HttpResponse and added use of it in the __init__

--- httptypes.py
+++ httptypes.py
@@ -6,10 +6,10 @@ Overview: This code contains a few types that will be used on working
 
 """
 import re
-import os
+from os import path
 
 # constants
-ROOT_DIR = 'webroot/'
+ROOT_DIR = 'webroot'
 
 
 class HttpRequest(object):
@@ -46,6 +46,21 @@ class HttpRequest(object):
             self.content_type = content_type
             self.content_length = int(content_length)
 
+    def __repr__(self):
+        return '{} {} {}'.format(self.method, self.resource, self.version)
+
+    def __str__(self):
+        first_line = '{} {} {}'.format(self.method, self.resource, self.version)
+        request = first_line + '\r\n'
+        if self.method == 'POST':
+            # add headers
+            request += 'Content-Length: {}\r\n'.format(self.content_length)
+            request += 'Content-Type: {}\r\n'.format(self.content_type)
+            request += '\r\n'
+            # add data
+            request += self.data
+        return request
+
 
 class HttpResponse(object):
     """
@@ -57,15 +72,48 @@ class HttpResponse(object):
         400: 'Bad Request',
         403: 'Forbidden',
         404: 'Not Found',
-        405: 'Method Not Allowed'
     }
 
-    def __init__(self, http_request):
+    def __init__(self, http_request, forbidden_resources=None):
         """
-        TODO: Take care of all code phrases
         This ctor forms an Http Response object using the http request
         :param http_request: HttpRequest object
         :type http_request: HttpRequest
+        :param forbidden_resources: Resources that the user is not supposed
+            to have access to.
         """
-        
+        self.code = HttpResponse.__get_code(http_request, forbidden_resources)
+        self.code_phrase = HttpResponse.code_phrases[self.code]
+        self.version = http_request.version
+
+    @staticmethod
+    def __get_code(http_request, forbidden_resources=None):
+        """
+        returns the matching code phrase for the request
+        :param http_request: http request
+        :type http_request: HttpRequest
+        :return: matching status code for the given request
+        """
+        if path.exists(path.join(ROOT_DIR, http_request.resource)):
+            # in case requested resource exists
+            # Check if resource path is harmful or not
+            if '..' in http_request.resource:
+                return 403  # Forbidden
+            if forbidden_resources and http_request.resource in \
+                    forbidden_resources:
+                return 403  # Forbidden
+
+            if http_request.method == 'GET':
+                return 200  # OK
+            elif http_request.method == 'POST':
+                # validate that the request contains all the headers
+                content_length = http_request.content_length
+                content_type = http_request.content_type
+                if not all((content_type, content_length)):
+                    return 400  # Bad Request
+        else:
+            # in case requested resource does not exist
+            return 404  # Not Found
+
+
 
